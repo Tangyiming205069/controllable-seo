5fcf4641351cdeb266b6d515f4a8aa94627107de
Tangyiming205069 <tangyiming510@gmail.com>
2025-04-27 22:46:51 -0700

    cuda device changable

--- script/ragroll.sh
+++ script/ragroll.sh
@@ -1,1 +1,3 @@
-python -m experiment.main --mode=suffix --dataset=ragroll --catalog="air compressor"
+export CUDA_VISIBLE_DEVICES=4
+# python -m experiment.main --mode=suffix --dataset=ragroll --catalog="air compressor"
+python -m experiment.main --mode=suffix --dataset=ragroll --catalog="automatic garden watering system"

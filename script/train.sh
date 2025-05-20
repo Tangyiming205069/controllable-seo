2770f0f5e3eda7030687d82d88a6a5e9adee134c
Tangyiming205069 <tangyimi@usc.edu>
2025-05-20 06:55:17 +0000

    final submission version

--- script/train.sh
+++ script/train.sh
@@ -1,12 +1,4 @@
-#!/bin/bash -l
 
-# SLURM SUBMIT SCRIPT
-#SBATCH --gres=gpu:a6000:1
-#SBATCH --nodelist=dill-sage
-#SBATCH --mem=32G
-#SBATCH --time=4000
-#SBATCH --output=/home/tangyimi/controllable-seo/slurm_output/ragroll/%j.out
-#SBATCH --job-name=ragroll
 
 # ["coffee_machines", "books", "cameras"]
 # ['llama-3.1-8b', 'llama-2-7b', 'vicuna-7b', 'mistral-7b', 'deepseek-7b']
